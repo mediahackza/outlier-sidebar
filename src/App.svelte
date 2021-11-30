d5a347a876f16bfb2f2dc772d9c672478359844a
Alastair Otter <alastair@mediahack.co.za>
2021-11-30 19:25:31 +0200

    Adding unemployment

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,21 @@
 <script>
   import MultiLineChart from './MultiLineChart.svelte'
+  import SidebarButton from './SidebarButton.svelte'
+  let href = 'https://theoutlier.co.za/economy/81582/sa-unemployment-tracker'
 </script>
 
 <main>
   <MultiLineChart />
+
+  <div class="unemployment">
+    <img src="/images/unemployment.png" alt="Unemployment Rate" />
+
+    <SidebarButton {href}>See all data</SidebarButton>
+  </div>
 </main>
+
+<style>
+  .unemployment {
+    margin-top: 100px;
+  }
+</style>
